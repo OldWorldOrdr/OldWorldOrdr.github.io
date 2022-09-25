96015ed874a69258cd3f8cab171233fc2a14c97e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-09-25 19:07:36 -0400

    add neofetch, improve script

--- debs/deb-compression.sh
+++
@@ -1,9 +0,0 @@
-COMP=gzip
-for i in *.deb; do
-    echo "$i"
-    mkdir "$i.extract"
-    dpkg-deb -R "$i" "$i.extract"
-    rm "$i"
-    dpkg-deb -b --root-owner-group "-Z$COMP" "$i.extract" "$i"
-    rm -rf "$i.extract"
-done
---
+++ debs/fixdeb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+case $1 in
+    *.deb)
+        echo "$1"
+        dpkg-deb -R "$1" "$1.extract"
+        rm "$1"
+        dpkg-deb -b --root-owner-group "-Zgzip" "$1.extract" "$1"
+        rm -rf "$1.extract"
+        exit 0
+    ;;
+esac
+
+for i in *.deb; do
+    echo "$i"
+    dpkg-deb -R "$i" "$i.extract"
+    rm "$i"
+    dpkg-deb -b --root-owner-group "-Zgzip" "$i.extract" "$i"
+    rm -rf "$i.extract"
+done
