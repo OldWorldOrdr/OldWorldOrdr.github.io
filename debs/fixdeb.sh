d2a150e5c3fe19f29da8268ca46f0b496425d776
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-09-28 10:09:28 -0400

    add sudo

--- debs/fixdeb.sh
+++ debs/fixdeb.sh
@@ -4,7 +4,7 @@ case $1 in
         echo "$1"
         dpkg-deb -R "$1" "$1.extract"
         rm "$1"
-        dpkg-deb -b --root-owner-group "-Zgzip" "$1.extract" "$1"
+        dpkg-deb -b --root-owner-group -Zgzip "$1.extract" "$1"
         rm -rf "$1.extract"
         exit 0
     ;;
@@ -14,6 +14,6 @@ for i in *.deb; do
     echo "$i"
     dpkg-deb -R "$i" "$i.extract"
     rm "$i"
-    dpkg-deb -b --root-owner-group "-Zgzip" "$i.extract" "$i"
+    dpkg-deb -b --root-owner-group -Zgzip "$i.extract" "$i"
     rm -rf "$i.extract"
 done
