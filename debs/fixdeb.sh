033005994caef6955ad46e6712c40061d53f4469
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-12-12 03:08:16 -0500

    recompile nextvi with -O2

--- debs/fixdeb.sh
+++ debs/fixdeb.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 case $1 in
-    *.deb)
-        echo "$1"
-        dpkg-deb -R "$1" "$1.extract"
-        rm "$1"
-        dpkg-deb -b --root-owner-group -Zgzip "$1.extract" "$1"
-        rm -rf "$1.extract"
-        exit 0
-    ;;
+  *.deb)
+    echo "$1"
+    dpkg-deb -R "$1" "$1.extract"
+    rm "$1"
+    dpkg-deb -b --root-owner-group -Zgzip "$1.extract" "$1"
+    rm -rf "$1.extract"
+    exit 0
+  ;;
 esac
 
 printf "Usage: %s <file.deb>\n" "$0"
