0b96e774637d62db6e54ce243c2da94f73997d0f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-09 18:37:16 -0500

    fix

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 cd "${0%/*}" || exit 1
-rm ./armv6/Packages*
-rm ./armv7/Packages*
-rm ./arm64/Packages*
-dpkg-scanpackages -m ./armv6/debs > ./armv6/Packages
-dpkg-scanpackages -m ./armv7/debs > ./armv7/Packages
-dpkg-scanpackages -m ./arm64/debs > ./arm64/Packages
-bzip2 -fks ./armv6/Packages
-bzip2 -fks ./armv7/Packages
-bzip2 -fks ./arm64/Packages
+(
+cd armv6 || exit 1
+dpkg-scanpackages -m ./debs > Packages
+bzip2 -fk Packages
+)
+(
+cd armv7 || exit 1
+dpkg-scanpackages -m ./debs > Packages
+bzip2 -fk Packages
+)
+(
+cd arm64 || exit 1
+dpkg-scanpackages -m ./debs > Packages
+bzip2 -fk Packages
+)
