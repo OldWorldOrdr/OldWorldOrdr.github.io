3ec1342a2572cdc69a7ad859193e7293c7fd4081
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-09 18:34:06 -0500

    move stuff

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 cd "${0%/*}" || exit 1
-rm Packages*
-dpkg-scanpackages -m ./debs > Packages
-bzip2 -fks Packages
+rm ./armv6/Packages*
+rm ./armv7/Packages*
+rm ./arm64/Packages*
+dpkg-scanpackages -m ./armv6/debs > ./armv6/Packages
+dpkg-scanpackages -m ./armv7/debs > ./armv7/Packages
+dpkg-scanpackages -m ./arm64/debs > ./arm64/Packages
+bzip2 -fks ./armv6/Packages
+bzip2 -fks ./armv7/Packages
+bzip2 -fks ./arm64/Packages
